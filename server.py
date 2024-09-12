41a1e1a553a82ead02739c24553be7111dcf7619
Utsa-Gayatri <dasutsa102@gmail.com>
2024-09-12 21:44:37 +0530

    Add files via upload

---
+++ server.py
@@ -0,0 +1,10 @@
+import flwr as fl
+
+strategy=fl.server.strategy.FedAvg()
+
+#start flower server
+fl.server.start_server (
+    server_address="0.0.0.0:8080",
+    config=fl.server.ServerConfig(num_rounds=3),
+    strategy=strategy,
+)
